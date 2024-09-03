ff4a27c34b3261a8064b23586a081fe9a6f5c92e
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-09-03 07:06:26 +0000

    Improve Game Sidebar Components.

---
+++ src/Controller/Api/Games/GetRoomsController.php
@@ -0,0 +1,35 @@
+<?php namespace App\Controller\Api\Games;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+class GetRoomsController extends AbstractController
+{
+    /** @var RepositoryInterface */
+    private $roomsRepository;
+    
+    public function __construct( RepositoryInterface $roomsRepository )
+    {
+        $this->roomsRepository    = $roomsRepository;
+    }
+    
+    public function __invoke( Request $request ): iterable
+    {
+        $rooms      = $this->roomsRepository->findAll();
+        $response   = [];
+        
+        foreach ( $rooms as $room ) {
+            $response[] = [
+                'id'        => $room->getId(),
+                'slug'      => $room->getSlug(),
+                'name'      => $room->getName(),
+                'players'   => $room->getPlayers(),
+                
+                'isPlaying' => false,
+            ];
+        }
+        
+        return $response;
+    }
+}
--- themes/GamePlatform_VelzonSaas/assets/js/games/application/interfaces/game-room.ts
+++ themes/GamePlatform_VelzonSaas/assets/js/games/application/interfaces/game-room.ts
@@ -4,6 +4,7 @@ import IPlayer from './player'
 interface IGameRoom
 {
     id: number;
+    isPlaying: boolean;
     game: IGame;
     slug: string;
     name: string;

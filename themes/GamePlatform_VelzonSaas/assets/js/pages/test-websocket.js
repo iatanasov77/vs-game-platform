fd064ba043cb02bd0d5e71f08656125146c3cc78
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-10-08 11:12:21 +0000

    Create Separate Websocket Test Pages Fifferent From WAMP/ZMQ Test With Autobahn.

--- themes/GamePlatform_VelzonSaas/assets/js/pages/test-websocket.js
+++ themes/GamePlatform_VelzonSaas/assets/js/pages/test-websocket.js
@@ -1,14 +1,8 @@
 window.MessagePublished = false;
 
-const autobahn      = require( 'autobahn-browser' );
 const url           = window.clientSettings.socketPublisherUrl;
 const currentUser   = window.currentUser;
-var socket;
-
-var connection = new autobahn.Connection({
-    url: url,
-    realm: 'realm1'
-});
+var socket          = ;
 
 function sendMessage( message )
 {
@@ -17,60 +11,9 @@ function sendMessage( message )
     }
 }
 
-
-function publishMessage( form, dto )
-{
-    window.MessagePublished = true;
-    
-    $.ajax({
-        type: "POST",
-        url: form.attr( 'action' ),
-        data: JSON.stringify( dto ),
-        dataType: 'json',
-        success: function( response )
-        {
-            window.MessagePublished = false;
-        },
-        error: function()
-        {
-            window.MessagePublished = false;
-            alert( "SYSTEM ERROR!!!" );
-        }
-    });    
-}
-
 function connectToTopic()
 {
-    connection.onopen = function ( session ) {
-    
-        // 1) subscribe to a topic
-        function onevent( args ) {
-            console.log( "Event:", args );
-            
-            if ( args[0].data ) {
-                $( '#client1_form_message' ).val( '' );
-                $( '#client2_form_message' ).val( '' );
-                
-                let data    = args[0].data;
-                let message = data.fromUser + ': ' + data.message;
-                let output;
-                
-                if ( currentUser == data.fromUser ) {
-                    output = '<span class="float-end">' + message + '</span><br /><br />';
-                } else {
-                    output = '<span class="float-start">' + message + '</span><br /><br />';
-                }
-                
-                $( '#ChatConsole' ).append( output ).animate( {scrollTop: $( '#ChatConsole' ).prop( "scrollHeight" ) }, 0 );
-            }
-        }
-        session.subscribe( 'chat', onevent );
-        
-        // 2) publish an event
-        session.publish( 'chat', ['Hello, world!'] );
-    };
-    
-    connection.open();
+    socket = new WebSocket( "ws://127.0.0.1:8080/" );
 }
 
 $( function()
@@ -89,7 +32,7 @@ $( function()
         }
         
         if ( ! window.MessagePublished ) {
-            publishMessage( form, dto );
+            sendMessage( dto );
         }
     });
     
@@ -103,7 +46,7 @@ $( function()
         }
         
         if ( ! window.MessagePublished ) {
-            publishMessage( form, dto );
+            sendMessage( dto );
         }
     });
 });

9f2f6533b0f488f49ddae8e9c89d0de4b294aa6c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-08-03 04:38:22 +0000

    Fix a Production Exception in Websocket Game Server.

--- config/applications/game-platform/bundles.php
+++ config/applications/game-platform/bundles.php
@@ -46,6 +46,8 @@ return [
     Vankosoft\PaymentBundle\VSPaymentBundle::class => ['all' => true],
     Vankosoft\CatalogBundle\VSCatalogBundle::class => ['all' => true],
 	
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],

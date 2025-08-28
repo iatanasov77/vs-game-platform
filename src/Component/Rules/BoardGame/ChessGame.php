3270c530388af81a9ed010fc3c05cdb998a1072d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-08-28 08:26:17 +0000

    Commit.

---
+++ src/Component/Rules/BoardGame/ChessGame.php
@@ -0,0 +1,10 @@
+<?php namespace App\Component\Rules\BoardGame;
+
+
+/**
+ * ChessEngine in C#: https://www.syedgakbar.com/projects/chess
+ */
+class ChessGame extends Game
+{
+    
+}
---
+++ src/Component/Rules/CardGame/ContractBridgeGame.php
@@ -0,0 +1,10 @@
+<?php namespace App\Component\Rules\CardGame;
+
+
+/**
+ * ContractBridgeGame Engine in Phython: https://github.com/lorserker/ben
+ */
+class ContractBridgeGame extends Game
+{
+    
+}

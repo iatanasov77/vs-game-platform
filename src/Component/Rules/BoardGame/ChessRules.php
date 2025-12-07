037fb38a05a8ac983fd66b747366afb15799335a
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-12-07 11:31:33 +0000

    Enable Chess Pieces only for My Player.

--- src/Component/Rules/BoardGame/ChessRules.php
+++ src/Component/Rules/BoardGame/ChessRules.php
@@ -734,6 +734,10 @@ class ChessRules
                 }
             }
         }
+        /*  
+        $pawnMoves  = \print_r( $moves->toArray(), true );
+        $this->logger->log( "Pawn Moves: {$pawnMoves}", 'EnginMoves' );
+        */
     }
     
     // calculate the possible moves for the knight piece and insert them into passed array

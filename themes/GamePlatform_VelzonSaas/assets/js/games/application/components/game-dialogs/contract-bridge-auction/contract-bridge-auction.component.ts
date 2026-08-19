d027339b86e1b245279af9d24bcc5296c4b460db
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-08-19 11:44:29 +0000

    Improve Contract Bridge Auction Dialog.

--- themes/GamePlatform_VelzonSaas/assets/js/games/application/components/game-dialogs/contract-bridge-auction/contract-bridge-auction.component.ts
+++ themes/GamePlatform_VelzonSaas/assets/js/games/application/components/game-dialogs/contract-bridge-auction/contract-bridge-auction.component.ts
@@ -9,7 +9,13 @@ import {
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { TranslateService } from '@ngx-translate/core';
 
-import { BidDto } from '@vankosoft/game-platform';
+import { BidTrump } from '@vankosoft/game-platform';
+import { ContractBridgeBidDto } from '@vankosoft/game-platform';
+import { PlayerPosition } from '@vankosoft/game-platform';
+
+import { AppStateService } from '../../../state/app-state.service';
+import { CardGameService } from '../../../services/websocket/card-game.service';
+
 import { ButtonComponent } from '../../shared/button/button.component';
 
 import cssString from './contract-bridge-auction.component.scss';
@@ -28,33 +34,88 @@ export class ContractBridgeAuctionComponent
 {
     @Output() closeModal: EventEmitter<any> = new EventEmitter();
     
-    lastBid: BidDto | null = null;
+    myPosition: PlayerPosition;
+    lastBid: ContractBridgeBidDto | null = null;
+    
+    bidValueChanged: boolean = false;
     bidValue: number | null = null;
-    bidTrump: string | null = null;
+    
+    bidTrumpChanged: boolean = false;
+    bidTrump: BidTrump | null = null;
     
     constructor(
-        @Inject( TranslateService ) private translateService: TranslateService
-    ) {}
+        @Inject( TranslateService ) private translateService: TranslateService,
+        @Inject( AppStateService ) private appStateService: AppStateService,
+        @Inject( CardGameService ) private wsService: CardGameService,
+    ) {
+        this.myPosition = this.appStateService.myPosition.getValue();
+    }
     
     dismissModal(): void
     {
         this.closeModal.emit();
     }
     
-    setBidValue( value: number ): void
+    onChangeBidValue(): void
     {
-        // alert( value );
-        this.bidValue = value;
+        this.bidValueChanged = true;
     }
     
-    setBidTrump( trump: string ): void
+    setBidValue( group: any ): void
     {
-        // alert( trump );
-        this.bidTrump = trump;
+        if ( ! this.bidValueChanged ) {
+            group.value = null;
+            this.bidValue = null;
+            this.bidTrump = null;
+        }
+        this.bidValueChanged = false;
+        // alert( this.bidValue );
+    }
+    
+    onChangeBidTrump(): void
+    {
+        this.bidTrumpChanged = true;
+    }
+    
+    setBidTrump( group: any ): void
+    {
+        if ( ! this.bidTrumpChanged ) {
+            group.value = null;
+            this.bidTrump = null;
+            this.bidValue = null;
+        }
+        this.bidTrumpChanged = false;
+        // alert( this.bidTrump );
+        
+        if ( this.bidTrump && ! this.bidValue ) {
+            this.bidValue = 1;
+        }
     }
     
     makeBid(): void
     {
+        let bidValue = this.bidValue ? this.bidValue : 0;
+        let bidTrump = this.bidTrump ? this.bidTrump : BidTrump.Pass;
+        
+        let bid: ContractBridgeBidDto = {
+            Player: this.myPosition,
+            Value: bidValue,
+            Trump: bidTrump,
+            NextBids: []
+        };
+        //alert( JSON.stringify( bid ) );
+        
+        this.doBid( bid );
         this.dismissModal();
+    }
+    
+    doBid( bid: ContractBridgeBidDto ): void
+    {
+        this.wsService.doBid( bid );
+        this.wsService.sendBid( bid );
+        /*  
+        let playerBids = this.appStateService.playerBids.getValue();
+        alert( playerBids.length );
+        */
     }
 }

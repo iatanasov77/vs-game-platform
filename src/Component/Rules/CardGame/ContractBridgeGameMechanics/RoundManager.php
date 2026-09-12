6042c1c83a586c93930acd2fe78f00392f56e4e7
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-09-12 14:25:09 +0000

    Create a Sore Manager for Contract Bridge.

--- src/Component/Rules/CardGame/ContractBridgeGameMechanics/RoundManager.php
+++ src/Component/Rules/CardGame/ContractBridgeGameMechanics/RoundManager.php
@@ -51,7 +51,7 @@ class RoundManager
         $this->eventDispatcher  = $eventDispatcher;
         
         $this->contractManager = new ContractManager( $this->game, $this->logger );
-        $this->tricksManager = new TricksManager( $this->game, $this->logger ); 
+        $this->tricksManager = new TricksManager( $this->game, $this->logger );
         $this->scoreManager = new ScoreManager( $this->game, $this->logger );
         
         $this->game->Deck = new Deck( $this->game->GameCode );
